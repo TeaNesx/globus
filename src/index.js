299c7962f4e3281fed0573bf8fdbcdc95e445899
TeaNesx <45711122+TeaNesx@users.noreply.github.com>
2022-08-15 21:00:20 +0200

    Adding style to offer element for mobile. Add slick slider to image slider. Add tab boxes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import "./main.scss";
 import slick from "slick-carousel";
+import {responsiveSlick} from "./js/slick";
+
 
 $('#burger-menu').on('click', function(){
     $('#navigation .list-unstyled, #login, #contact, #newsletter').slideToggle(300).toggleClass('active');
 });
+
+responsiveSlick();
